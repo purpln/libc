8c821144ac82a0a22d2d74a562b3c00d9440e650
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2025-05-23 13:03:56 +0300

    no message

--- Sources/System/include/platform_shims.h
+++ Sources/System/include/platform_shims.h
@@ -17,11 +17,3 @@ static inline char * _Nonnull _platform_shims_dirent_d_name(struct dirent * _Non
     return entry->d_name;
 }
 #endif
-
-#if __ANDROID__
-#include <android/log.h>
-
-static inline int android_log(android_LogPriority priority, const char* tag, const char* message) {
-    return __android_log_print(priority, tag, "%s", message);
-}
-#endif
