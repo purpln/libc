10dc51ef6b5df18d4c0d7a3eaaaf783a9ee556df
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2026-07-03 14:20:53 +0300

    no message

--- Sources/LibCExternal/include/os_android.h
+++ Sources/LibCExternal/include/os_android.h
@@ -1,1 +1,8 @@
 #pragma once
+
+#include <sys/epoll.h>
+
+static inline uint32_t _getConst_EPOLLEXCLUSIVE(void) { return EPOLLEXCLUSIVE; }
+static inline uint32_t _getConst_EPOLLWAKEUP(void) { return EPOLLWAKEUP; }
+static inline uint32_t _getConst_EPOLLONESHOT(void) { return EPOLLONESHOT; }
+static inline uint32_t _getConst_EPOLLET(void) { return EPOLLET; }
